16d4bb9fc48b69dcba1e94091de6442311daf3cb
Bruno Ferrari <brunofvincensi@gmail.com>
2025-03-24 20:14:20 -0300

    setup

---
+++ backend/petconnect/src/test/java/com/br/petconnect/PetconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.petconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
